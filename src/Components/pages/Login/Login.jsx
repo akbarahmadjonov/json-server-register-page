31e41e14870930c9aaeb4e53724abc3272958388
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-02-08 11:59:41 +0500

    Add Yup validation

--- src/Components/pages/Login/Login.jsx
+++ src/Components/pages/Login/Login.jsx
@@ -3,6 +3,8 @@ import { useState, useRef } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../../Context/AuthContext";
 import { UserContext } from "../../../Context/UserContext";
+import * as Yup from "yup";
+import { Formik, ErrorMessage, Field } from "formik";
 
 export const Login = () => {
   const { setToken } = useContext(AuthContext);
@@ -28,6 +30,19 @@ export const Login = () => {
       .catch((err) => console.log(err));
   };
 
+  const validateSchema = Yup.object({
+    emailRef: Yup.string()
+      .required("Email is required and must be a valid"),
+    passwordRef: Yup.string()
+      .min(3, "Password must be a 3 characters or long")
+      .max(15, "Password must be a 15 characters or small")
+      .required("Password can't be blank"),
+  });
+  const initialValues = {
+    emailRef: "",
+    passwordRef: "",
+  };
+
   return (
     <>
       <main className="app__content my-3">
@@ -38,35 +53,47 @@ export const Login = () => {
             professional world.
           </p>
         </div>
-        <form onSubmit={handleSubmit} className="login__form">
-          <div className="form__input--floating">
-            <label className="form__label--floating" id="label--email">
-              Email or Phone
-            </label>
-            <input ref={emailRef} type="email" id="input--email" />
-          </div>
-          <div className="form__input--floating">
-            <label className="form__label--floating" id="label--password">
-              Password
-            </label>
-            <input ref={passwordRef} id="input--password" type="password" />
-          </div>
-          <div className="login__form_action_container login__form_action_container--multiple-actions">
-            <button
-              className="btn__primary--large from__button--floating"
-              type="submit"
-              aria-label="Sign in"
-            >
-              Sign in
-            </button>
-            <button
-              className="btn__secondary--large from__button--floating"
-              aria-label="Cancel"
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
+        <Formik
+          validationSchema={validateSchema}
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+        >
+          <form className="login__form">
+            <div className="form__input--floating">
+              <label className="form__label--floating" id="label--email">
+                Email or Phone
+              </label>
+              <Field name="emailRef" type="email" id="input--email" />
+              <span className="error">
+                <ErrorMessage name="emailRef" />
+              </span>
+            </div>
+            <div className="form__input--floating">
+              <label className="form__label--floating" id="label--password">
+                Password
+              </label>
+              <Field name="passwordRef" id="input--password" type="password" />
+              <span className="text-danger">
+                <ErrorMessage name="passwordRef" />
+              </span>
+            </div>
+            <div className="login__form_action_container login__form_action_container--multiple-actions">
+              <button
+                className="btn__primary--large from__button--floating"
+                type="submit"
+                aria-label="Sign in"
+              >
+                Sign in
+              </button>
+              <button
+                className="btn__secondary--large from__button--floating"
+                aria-label="Cancel"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </Formik>
         <div className="footer-app-content-actions">
           <div>
             <a>Forgot password?</a>
--- src/Components/pages/Register/Register.jsx
+++ src/Components/pages/Register/Register.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Context/AuthContext";
 import { UserContext } from "../../../Context/UserContext";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import { Formik, ErrorMessage, Field } from "formik";
 
 export const Register = () => {
   const { setToken } = useContext(AuthContext);
@@ -35,6 +37,23 @@ export const Register = () => {
       .catch((err) => console.log(err));
   };
 
+  const validateSchema = Yup.object({
+    firstNameRef: Yup.string().required("First name is required"),
+    lastNameRef: Yup.string().required("Last name is required"),
+    emailRef: Yup.string().required("Email is required"),
+    passwordRef: Yup.string()
+      .min(3, "Password must be 3 characters or long")
+      .max(15, "Password must be 3 characters or long")
+      .required("Password can't be blank "),
+  });
+
+  const initalValues = {
+    firstNameRef: "",
+    lastNameRef: "",
+    emailRef: "",
+    passwordRef: "",
+  };
+
   return (
     <>
       <main className="app__content my-3">
@@ -42,39 +61,57 @@ export const Register = () => {
           <h1 className="header__content__heading">Welcome, Register</h1>
           <p className="header__content__subheading">Meet with the world!</p>
         </div>
-        <form onSubmit={handleSubmit} className="login__form">
-          <div className="form__input--floating">
-            <label className="form__label--floating">First name</label>
-            <input ref={firstNameRef} type="text" />
-          </div>
-          <div className="form__input--floating">
-            <label className="form__label--floating">Last name</label>
-            <input ref={lastNameRef} type="text" />
-          </div>
-          <div className="form__input--floating">
-            <label className="form__label--floating">Email or Phone</label>
-            <input ref={emailRef} type="email" />
-          </div>
-          <div className="form__input--floating">
-            <label className="form__label--floating">Password</label>
-            <input ref={passwordRef} id="input--password" type="password" />
-          </div>
-          <div className="login__form_action_container login__form_action_container--multiple-actions">
-            <button
-              className="btn__primary--large from__button--floating"
-              type="submit"
-              aria-label="Sign in"
-            >
-              Sign in
-            </button>
-            <button
-              className="btn__secondary--large from__button--floating"
-              aria-label="Cancel"
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
+        <Formik
+          validationSchema={validateSchema}
+          initialValues={initalValues}
+          onSubmit={handleSubmit}
+        >
+          <form className="login__form">
+            <div className="form__input--floating">
+              <label className="form__label--floating">First name</label>
+              <Field name="firstNameRef" type="text" />
+              <span className="error">
+                <ErrorMessage name="firstNameRef" />
+              </span>
+            </div>
+            <div className="form__input--floating">
+              <label className="form__label--floating">Last name</label>
+              <Field name="lastNameRef" type="text" />
+              <span className="error">
+                <ErrorMessage name="lastNameRef" />
+              </span>
+            </div>
+            <div className="form__input--floating">
+              <label className="form__label--floating">Email or Phone</label>
+              <Field name="emailRef" type="email" />
+              <span className="error">
+                <ErrorMessage name="emailRef" />
+              </span>
+            </div>
+            <div className="form__input--floating">
+              <label className="form__label--floating">Password</label>
+              <Field name="passwordRef" id="input--password" type="password" />
+              <span className="error">
+                <ErrorMessage name="passwordRef" />
+              </span>
+            </div>
+            <div className="login__form_action_container login__form_action_container--multiple-actions">
+              <button
+                className="btn__primary--large from__button--floating"
+                type="submit"
+                aria-label="Sign in"
+              >
+                Sign in
+              </button>
+              <button
+                className="btn__secondary--large from__button--floating"
+                aria-label="Cancel"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </Formik>
         <div className="footer-app-content-actions">
           <div>
             <Link to="/">Forgot password?</Link>
